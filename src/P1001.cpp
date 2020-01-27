3bb887e3c7e57b94e130a2df4d0c95d615d8095e
秋雨落 <i@rain.cx>
2020-01-27 10:29:15 +0800

    init
    Signed-off-by: 秋雨落 <i@rain.cx>

---
+++ src/P1001.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    long a = 0;
+    long b = 0;
+    scanf("%d%d", &a, &b);
+    printf("%d", a + b);
+    return 0;
+}
---
+++ src/P1002.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int xSize = 0;
+    int ySize = 0;
+    int locX = 0;
+    int locY = 0;
+    scanf("%d %d %d %d", &xSize, &ySize, &locX, &locY);
+
+    int board[xSize][ySize];
+
+    return 0;
+}
